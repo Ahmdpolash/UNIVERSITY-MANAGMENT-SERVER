26c7bab19a8fb92c65ba5d0638a8b80e6cef5dcc
Ahmdpolash <ahmedpolash732@gmail.com>
2024-09-27 21:32:38 +0600

    Create Semester Registration

--- src/app/modules/SemesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/SemesterRegistration/semesterRegistration.controller.ts
@@ -1,21 +1,28 @@
 import catchAsync from "../../utils/catchAsync";
 
+import { SemesterRegistrationService } from "./semesterRegistration.services";
 
 // create new semester
-const createSemesterRegistration = catchAsync(async (req, res) => { });
+const createSemesterRegistration = catchAsync(async (req, res) => {
+  const result =await SemesterRegistrationService.createSemesterRegistrationIntoDB(
+      req.body
+    );
 
+  res.status(200).json({
+    success: true,
+    message: "Semester registration created successfully",
+    data: result,
+  });
+});
 
 // get all semesters
-const getAllSemesterRegistrations = catchAsync(async (req, res) => { });
+const getAllSemesterRegistrations = catchAsync(async (req, res) => {});
 
-
-// get single registered semester by id  (using id from url parameter)  
-const getSingleSemesterRegistration = catchAsync(async (req, res) => { });
-
+// get single registered semester by id  (using id from url parameter)
+const getSingleSemesterRegistration = catchAsync(async (req, res) => {});
 
 // update registered semester
-const updateSemesterRegistration = catchAsync(async (req, res) => { });
-
+const updateSemesterRegistration = catchAsync(async (req, res) => {});
 
 // delete registered semester
 const deleteSemesterRegistration = catchAsync(async (req, res) => {});
--- src/app/modules/SemesterRegistration/semesterRegistration.services.ts
+++ src/app/modules/SemesterRegistration/semesterRegistration.services.ts
@@ -1,29 +1,58 @@
-
-
+import httpStatus from "http-status";
+import { AcademicSemester } from "../academicSemester/academicSemester.modal";
+import { TSemesterRegistration } from "./semesterRegistration.interface";
+import { SemesterRegistration } from "./semesterRegistration.model";
+import AppError from "../../errors/AppError";
 
 // register a new semester
-const createSemesterRegistrationIntoDB = async () => { };
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesterRegistration
+) => {
+  /**
+   * Step1: Check if there any registered semester that is already 'UPCOMING'|'ONGOING'
+   * Step2: Check if the semester is exist
+   * Step3: Check if the semester is already registered!
+   * Step4: Create the semester registration
+   */
+
+  const academicSemester = payload?.academicSemester;
+
+  // check if the semester is already exist
+  const isAcademicSemesterExists = await AcademicSemester.findById(
+    academicSemester
+  );
+
+  if (!isAcademicSemesterExists) {
+    throw new AppError(httpStatus.NOT_FOUND, "Academic semester not found");
+  }
+
+  // check if the semester is already registered!
+  const isSemesterRegistrationExists = await SemesterRegistration.findOne({
+    academicSemester,
+  });
+
+  if (isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "This semester is already registered"
+    );
+  }
+
+  const result = await SemesterRegistration.create(payload);
+  return result;
+};
 
 //get all registered semester
-const getAllSemesterRegistrationsFromDB = async () => { };
+const getAllSemesterRegistrationsFromDB = async () => {};
 
 //get single semester registration
-const getSingleSemesterRegistrationsFromDB = async () => { };
-
+const getSingleSemesterRegistrationsFromDB = async () => {};
 
 // update semester registration
-const updateSemesterRegistrationIntoDB = async () => { };
-
+const updateSemesterRegistrationIntoDB = async () => {};
 
 // delete semester registration
 const deleteSemesterRegistrationFromDB = async () => {};
-
-
-
-
-
-
-
 
 export const SemesterRegistrationService = {
   createSemesterRegistrationIntoDB,
